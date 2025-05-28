cfe73fe6e15609f1a69395a18629a31725703e2c
joaovnpt <joaovitoguiotti8@gmail.com>
2025-05-28 12:35:21 -0300

    FIX: Update recipient tests to use Ingredient instance as foreign key #4

--- core/establishment/tests/test_recipient.py
+++ core/establishment/tests/test_recipient.py
@@ -2,16 +2,24 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from core.establishment.models import Recipient
+from core.establishment.models import Ingredient
 
 
 class TestRecipientAPI(APITestCase):
     def setUp(self):
+        self.ingredient = Ingredient.objects.create(
+            name='Acai',
+            portion=100,
+            stock=50,
+            price=5.00,
+            unit='g',
+        )
         self.recipient = Recipient.objects.create(
             quantity_ml="10",
             price="15.00",
             title="Medium recipient",
             stock="5",
-            content="2",
+            content=self.ingredient,
         )
 
     def test_recipient_creation(self):
@@ -19,11 +27,11 @@ class TestRecipientAPI(APITestCase):
         response = self.client.post(
             url,
             {
+                "title": "Large recipient",
                 "quantity_ml": "20",
                 "price": "30.00",
-                "title": "Large recipient",
                 "stock": "10",
-                "content": "5",
+                "content": self.ingredient.id,
             },
         )
         self.assertEqual(response.status_code, 201)
@@ -31,9 +39,9 @@ class TestRecipientAPI(APITestCase):
         self.assertEqual(Recipient.objects.get(id=2).title, "Large recipient")
 
     def test_recipient_list(self):
-        url = reverse("api:recipients")
+        url = reverse("api:recipients-list")
         response = self.client.get(url)
-        self.assertEqual(response.status.code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["title"], "Medium recipient")
 
@@ -52,9 +60,9 @@ class TestRecipientAPI(APITestCase):
                 "price": "35.00",
                 "title": "Large recipient",
                 "stock": "12",
-                "content": "5",
+                "content": self.ingredient.id,
             },
         )
         self.assertEqual(response.status_code, 200)
         self.recipient.refresh_from_db()
-        self.assertEqual(self.recipient.name, 'Large recipient')
+        self.assertEqual(self.recipient.title, 'Large recipient')
