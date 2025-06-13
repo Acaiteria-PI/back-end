6618389d6eee4848c86b8148ae065116413fca27
Jaotarzan <joaovssouza59@gmail.com>
2025-06-13 15:51:55 -0300

    REFATORAÇÃO: Remover linha em branco e reorganizar importações no admin.py

--- core/establishment/admin.py
+++ core/establishment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from core.establishment.models import Establishment, Combo, FinalCup, Ingredient, Recipient 
 # Register your models here.
+
+admin.site.register(Establishment)
+admin.site.register(Combo)
+admin.site.register(FinalCup)
+admin.site.register(Ingredient)
+admin.site.register(Recipient)
