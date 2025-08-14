8479b8eb8131d062c4d47b60ab1779709dba3928
Jaotarzan <joaovssouza59@gmail.com>
2025-08-14 09:55:30 -0300

    FEAT: Registrar modelo User com classe personalizada UserAdmin no admin do Django

--- core/users/admin.py
+++ core/users/admin.py
@@ -1,5 +1,34 @@
+# core/users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
 from core.users.models import User
 
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["-date_joined"]
+    list_display = ("email", "name", "registration", "is_management", "is_staff", "is_active")
+    list_filter = ("is_management", "is_staff", "is_active", "Establishment")
+    search_fields = ("email", "name", "registration")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Informações pessoais"), {"fields": ("name", "registration", "Establishment")}),
+        (_("Permissões"), {"fields": ("is_active", "is_staff", "is_superuser", "is_management", "groups", "user_permissions")}),
+        (_("Datas importantes"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "name", "registration", "Establishment", "password1", "password2", "is_active", "is_staff", "is_superuser", "is_management"),
+        }),
+    )
+
+    # Campo usado como identificador
+    add_form_template = None
+    filter_horizontal = ("groups", "user_permissions")
+
+
