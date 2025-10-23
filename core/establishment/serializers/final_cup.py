63eb6c1758930f912d65637a4c7ff5d8f71e57bc
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-23 10:45:50 -0300

    FEAT: Add ingredient_data field to final_cup serializer #16

--- core/establishment/serializers/final_cup.py
+++ core/establishment/serializers/final_cup.py
@@ -1,10 +1,13 @@
+from rest_framework import serializers
+from core.establishment.models import FinalCup, Ingredient
 from rest_framework.serializers import ModelSerializer
-
-from core.establishment.models import FinalCup
+from core.establishment.serializers import IngredientSerializer
 
 
 class FinalCupSerializer(ModelSerializer):
+    ingredient_data = IngredientSerializer(source="ingredient", read_only=True, many=True)
+    
     class Meta:
         model = FinalCup
-        fields = ["name", "price", "recipient", "ingredient"]
+        fields = ["name", "price", "recipient", "ingredient", "ingredient_data"]
         read_only_fields = ["id"]
