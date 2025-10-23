ce5aa1333b1a6bb9defba83f5d1b54ca3491b9ff
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-23 11:29:51 -0300

    FEAT: Add recipient_data field on final_cup serializer #16

--- core/establishment/serializers/final_cup.py
+++ core/establishment/serializers/final_cup.py
@@ -1,13 +1,13 @@
-from rest_framework import serializers
-from core.establishment.models import FinalCup, Ingredient
+from core.establishment.models import FinalCup
 from rest_framework.serializers import ModelSerializer
-from core.establishment.serializers import IngredientSerializer
+from core.establishment.serializers import IngredientSerializer, RecipientSerializer
 
 
 class FinalCupSerializer(ModelSerializer):
     ingredient_data = IngredientSerializer(source="ingredient", read_only=True, many=True)
+    recipient_data = RecipientSerializer(source="recipient", read_only=True)
     
     class Meta:
         model = FinalCup
-        fields = ["name", "price", "recipient", "ingredient", "ingredient_data"]
+        fields = ["name", "price", "recipient", "recipient_data", "ingredient", "ingredient_data"]
         read_only_fields = ["id"]
