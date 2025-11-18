edbabc95ffeb82acfe64e6b30dfb60aaa8b71d5f
joaovnpt <joaovitorguiotti8@gmail.com>
2025-11-18 13:19:42 -0300

    FIX: Add "id" as a combo field

--- core/establishment/serializers/combo.py
+++ core/establishment/serializers/combo.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
+from core.establishment.serializers.final_cup import FinalCupSerializer
 from core.establishment.models import Combo
 
 class ComboSerializer(ModelSerializer):
+    final_cup_data = FinalCupSerializer(source='final_cup', read_only=True, many=True)
+
     class Meta:
         model = Combo
-        fields = ['name', 'price', 'final_cup']
+        fields = ['id', 'name', 'price', 'final_cup', 'final_cup_data']
         read_only_fields = ['id']
