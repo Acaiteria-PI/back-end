9fc96247071e4ea093443af0530ff7e7df22f249
guiottijoao <joaovitorguiotti8@gmail.com>
2025-11-28 13:45:23 -0300

    FEAT: Add search filters #20

--- core/establishment/views/final_cup.py
+++ core/establishment/views/final_cup.py
@@ -3,8 +3,12 @@ from rest_framework.viewsets import ModelViewSet
 from core.establishment.models import FinalCup
 from core.establishment.serializers import FinalCupSerializer
 
+from rest_framework.filters import SearchFilter
+
 
 class FinalCupViewSet(ModelViewSet):
     queryset = FinalCup.objects.all()
     serializer_class = FinalCupSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ["name", "ingredient"]
     http_method_names = ["get", "post", "put", "delete"]
--- core/orders/serializers.py
+++ core/orders/serializers.py
@@ -1,15 +1,16 @@
 from core.orders.models import Order, OrderItem
-from core.establishment.serializers import ComboSerializer, FinalCupSerializer, CustomCupSerializer, EstablishmentSerializer
+from core.establishment.models import Combo, CustomCup, FinalCup
+from core.establishment.serializers import FinalCupSerializer, CustomCupSerializer, EstablishmentSerializer
 from core.users.serializers import UserSerializer
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 
 class OrderSerializer(ModelSerializer):
-
+    responsible_person_data = UserSerializer(source='responsible_person', read_only=True)
     class Meta:
         model = Order
-        fields = ["id", "customer", "order_date", "total_amount", 'establishment', "status", "is_paid", "responsible_person"]
-        read_only_fields = ["id", "establishment", "responsible_person", "order_date", "total_amount"]
+        fields = ["id", "customer", "order_date", "total_amount", 'establishment', "status", "is_paid", "responsible_person", "responsible_person_data"]
+        read_only_fields = ["id", "establishment", "responsible_person", "order_date", "total_amount", "responsible_person_data"]
         
         
 class OrderDetailSerializer(ModelSerializer):
@@ -23,11 +24,11 @@ class OrderDetailSerializer(ModelSerializer):
         read_only_fields = ["id"]
         
 class OrderItemSerializer(ModelSerializer):
-    order = OrderSerializer(read_only=True)
-    combo = ComboSerializer(read_only=True)
-    final_cup = FinalCupSerializer(read_only=True)
-    custom_cup = CustomCupSerializer(read_only=True)
-    
+    order = PrimaryKeyRelatedField(queryset=Order.objects.all())
+    combo = PrimaryKeyRelatedField(queryset=Combo.objects.all(), required=False, allow_null=True)
+    final_cup = PrimaryKeyRelatedField(queryset=FinalCup.objects.all(), required=False, allow_null=True)
+    custom_cup = PrimaryKeyRelatedField(queryset=CustomCup.objects.all(), required=False, allow_null=True)
+
     class Meta:
         model = OrderItem
         fields = ["id", "order", "type", "combo", "final_cup", "custom_cup", "quantity", "unit_price", "total_price"]
