27704586e86c39058af6971e62ec22b12f866391
guiottijoao <joaovitorguiotti8@gmail.com>
2026-01-31 18:06:57 -0300

    FEAT: Add establlishment_data field in user serializer

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from core.users.models import User
+from core.establishment.serializers import EstablishmentSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    establishment_data = EstablishmentSerializer(source='establishment', read_only=True)
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'registration', 'establishment', 'is_management']
+        fields = ['id', 'name', 'email', 'registration', 'establishment', 'establishment_data', 'is_management']
