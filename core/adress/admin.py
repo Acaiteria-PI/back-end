065f5447fd9c8bf2d222595fce47177c803fdbc3
jaotarzan <joaovssouza59@gmail.com>
2026-02-03 13:26:49 -0300

    FEAT: registrar modelos Address e Contact no admin

--- core/adress/admin.py
+++ core/adress/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from core.adress.models import Address
 
 # Register your models here.
+admin.site.register(Address)
--- core/supplier/admin.py
+++ core/supplier/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.supplier.models import Supplier, Contact
 
 # Register your models here.
+
+admin.site.register(Supplier)
+admin.site.register(Contact)
