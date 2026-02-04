ce74b2d27fd74c86402836abeaa4d9f5aaa8798f
guiottijoao <joaovitorguiotti8@gmail.com>
2026-02-04 03:05:48 -0300

    FEAT: add address details field and contact details field in supplier serializer #26

--- core/supplier/serializers.py
+++ core/supplier/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from core.supplier.models import Supplier, Contact
+from core.adress.serializers import AddressSerializer
 
-class SupplierSerializer(ModelSerializer):
-    class Meta:
-        model = Supplier
-        fields = ['id', 'legal_name', 'name', 'document', 'is_active', 'type', 'contact', 'address']
-        depth = 1
-        
 class ContactSerializer(ModelSerializer):
     class Meta:
         model = Contact
         fields = ['id', 'business_email', 'financial_email', 'phone_number', 'whatsapp_number']
+
+class SupplierSerializer(ModelSerializer):
+    address_data = AddressSerializer(source='address', read_only=True)
+    contact_data= ContactSerializer(source='contact', read_only=True)
+    class Meta:
+        model = Supplier
+        fields = ['id', 'legal_name', 'name', 'document', 'is_active', 'type', 'contact', 'address', 'contact_data', 'address_data']
+        
